c59ba48c013b9e8bc112d4a2bf7b53d5e1224c8b
Pritiraj Partho <dpartho5616@gmail.com>
2025-09-13 16:32:06 +0600

    try-set-data

--- interact.js
+++ interact.js
@@ -1,17 +1,15 @@
 // my local api key is below
 const apiKey = "350c92110068b8850f7e74c297509483"; 
-const city = "london";
-
 // more html dom elements
-const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('search-button');
 const currentLocationButton = document.getElementById('current-location-button');
-const cityTitle = document.getElementById('city');
 const weatherContainer = document.querySelector('.weather');
-const rightPanel = document.querySelector('.right-panel');
+const weatherCard = document.querySelector('.left-panel');
+const forecastCard = document.querySelector('.right-panel');
 
 
-function getWeatherDetails(name, country, sunrise, sunset, id, icon, description, temp, humidity, pressure, feels_like, temp_min, temp_max, speed, deg, gust, all){  
+function getWeatherDetails(name, country, lat, lon, sunrise, sunset, id, icon, description, temp, humidity, pressure, feels_like, temp_min, temp_max, speed, deg, gust, all){  
     let forcast_api_url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;    
     weather_api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;  
     
@@ -40,6 +38,31 @@ function getWeatherDetails(name, country, sunrise, sunset, id, icon, description
         "December"
     ]
 
+    fetch(weather_api_url)
+    .then(response => response.json())
+    .then(data => {
+        let date = new Date();
+        weatherCard.innerHTML = `
+        
+        `
+    })
+    .catch(err => alert(`weather error: ${err}`));
+
+    // fetch forcast
+    fetch(forcast_api_url)
+    .then(response => response.json())
+    .then(data => {
+         let uniqueForecastDays = [];
+         let fiveDaysForecast = data.list.filter(forecast => {
+             let forecastDate = new Date(forecast.dt_txt).getDate();
+             if(!uniqueForecastDays.includes(forecastDate)){
+                 return uniqueForecastDays.push(forecastDate);
+             }
+         })
+        console.log(fiveDaysForecast);
+    })
+    .catch(err => alert(`forecast error: ${err}`));
+
 }
 
 
@@ -52,15 +75,16 @@ function getWeather(){
     fetch(geocoding_api_url)
     .then(response => response.json())
     .then(data =>{
-        let{name} = data;
+        let{lat, lon} = data.coord;
+        let{name,} = data;
         let{temp, humidity, pressure, feels_like, temp_min, temp_max} = data.main;
         let{country, sunrise, sunset, id} = data.sys;
         let{speed, deg, gust} = data.wind;
         let{main, description, icon} = data.weather[0];
         let{all} = data.clouds;
-        getWeatherDetails(name, country, sunrise, sunset, id, icon, description, temp, humidity, pressure, feels_like, temp_min, temp_max, speed, deg, gust, all);
+        getWeatherDetails(name, country, lat, lon, sunrise, sunset, id, icon, description, temp, humidity, pressure, feels_like, temp_min, temp_max, speed, deg, gust, all);
         // let{visibility} = data.visibility;
-        // console.log(visibility);
+        // console.log(data);
     })
     .catch(err => alert(`Something went wrong: ${err}`));
 }
